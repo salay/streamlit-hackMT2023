1b893e49e05fea2306dfb38247034fea11269b81
Siri Alay <siri.alay1@gmail.com>
2023-01-28 15:31:42 -0600

    wip

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,16 @@
 import streamlit as st
 from src.data_pipeline import read_subreddit_data_from_s3
 
-st.title("Reddit Sentiment Analysis")
+st.write("helloooooo")
 
-df = read_subreddit_data_from_s3("test", "test-file-name")
+# st.title("Reddit Sentiment Analysis")
 
-st.dataframe(df)
+# df = read_subreddit_data_from_s3("test", "test-file-name")
 
-# df = pd.read_csv("reddit_sentiment_analysis.csv")
-subreddit_title = st.text_input('Subreddit title', 'Enter a title')
-st.write("Subreddit title is ", subreddit_title)
+# st.dataframe(df)
 
-st.write(df.loc[df['Title'] == subreddit_title])
+# # df = pd.read_csv("reddit_sentiment_analysis.csv")
+# subreddit_title = st.text_input('Subreddit title', 'Enter a title')
+# st.write("Subreddit title is ", subreddit_title)
+
+# st.write(df.loc[df['Title'] == subreddit_title])
